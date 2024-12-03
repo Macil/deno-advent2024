79d80723b03a217e99625a3f88a28582ca69e9a1
Chris Cowan <agentme49@gmail.com>
2024-12-03 00:36:51 -0800

    day 3 part 2

--- day_3.ts
+++ day_3.ts
@@ -8,13 +8,25 @@ function part1(input: string): number {
     .reduce((a, b) => a + b, 0);
 }
 
-// function part2(input: string): number {
-//   throw new Error("TODO");
-// }
+function part2(input: string): number {
+  let mulInstructionsEnabled = true;
+  let sum = 0;
+  const matches = input.matchAll(/do\(\)|don't\(\)|mul\((\d+),(\d+)\)/g);
+  for (const m of matches) {
+    if (m[0] === "do()") {
+      mulInstructionsEnabled = true;
+    } else if (m[0] === "don't()") {
+      mulInstructionsEnabled = false;
+    } else if (mulInstructionsEnabled) {
+      sum += Number(m[1]) * Number(m[2]);
+    }
+  }
+  return sum;
+}
 
 if (import.meta.main) {
   runPart(2024, 3, 1, part1);
-  // runPart(2024, 3, 2, part2);
+  runPart(2024, 3, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -25,6 +37,11 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 161);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(
+    part2(
+      "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+    ),
+    48,
+  );
+});
