11a7b953f84f111bff71ad5cf33baaeb42b98f69
Chris Cowan <agentme49@gmail.com>
2024-12-03 00:38:10 -0800

    formatting tweak

--- day_3.ts
+++ day_3.ts
@@ -29,12 +29,13 @@ if (import.meta.main) {
   runPart(2024, 3, 2, part2);
 }
 
-const TEST_INPUT = `\
-xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-`;
-
 Deno.test("part1", () => {
-  assertEquals(part1(TEST_INPUT), 161);
+  assertEquals(
+    part1(
+      "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+    ),
+    161,
+  );
 });
 
 Deno.test("part2", () => {
