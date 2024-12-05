91e6af4bbb82c12c9daf298c7e93224f1b5c807b
Chris Cowan <agentme49@gmail.com>
2024-12-05 00:48:19 -0800

    day 2 part 2

--- day_2.ts
+++ day_2.ts
@@ -32,14 +32,28 @@ function part1(input: string): number {
   return safeReports.length;
 }
 
-// function part2(input: string): number {
-//   const reports = parse(input);
-//   throw new Error("TODO");
-// }
+function checkSafeUsingDampener(report: number[]): boolean {
+  if (checkSafe(report)) {
+    return true;
+  }
+  for (let i = 0; i < report.length; i++) {
+    const modifiedReport = [...report.slice(0, i), ...report.slice(i + 1)];
+    if (checkSafe(modifiedReport)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function part2(input: string): number {
+  const reports = parse(input);
+  const safeReports = reports.filter(checkSafeUsingDampener);
+  return safeReports.length;
+}
 
 if (import.meta.main) {
   runPart(2024, 2, 1, part1);
-  // runPart(2024, 2, 2, part2);
+  runPart(2024, 2, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -55,6 +69,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 2);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 4);
+});
