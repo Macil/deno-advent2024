0658cd1a1778b918620f294888d2dbe2535793dd
Chris Cowan <agentme49@gmail.com>
2024-12-31 20:36:32 -0800

    day 6 part 2

--- day_6.ts
+++ day_6.ts
@@ -1,43 +1,151 @@
 import { assertEquals } from "@std/assert";
 import { runPart } from "@macil/aocd";
-import { CharacterGrid } from "./grid/grid.ts";
+import { ArrayGrid, CharacterGrid, FixedSizeGrid, Grid } from "./grid/grid.ts";
+import { Location } from "./grid/location.ts";
 import { clockwise, Direction } from "./grid/direction.ts";
+import { Vector } from "./grid/vector.ts";
+
+function* guardLocations(
+  grid: FixedSizeGrid<string>,
+  guardLocation: Location,
+  guardDirection: Direction,
+): Generator<[Location, Direction]> {
+  while (grid.isInBounds(guardLocation)) {
+    yield [guardLocation, guardDirection];
+    const nextLocation = guardLocation.relative(
+      guardDirection,
+      1,
+    );
+    if (grid.get(nextLocation) === "#") {
+      guardDirection = clockwise(guardDirection);
+    } else {
+      guardLocation = nextLocation;
+    }
+  }
+}
 
 function part1(input: string): number {
   const grid = CharacterGrid.fromString(input);
   const visitedLocations = new Set<string>();
-  let currentGuardDirection: Direction = "up";
-  let currentGuardLocation = grid.valuesWithLocations()
+  const guardStartLocation = grid.valuesWithLocations()
     .find(({ value }) => value === "^")?.location;
-  if (!currentGuardLocation) {
+  if (!guardStartLocation) {
     throw new Error("No guard location found");
   }
-  while (grid.isInBounds(currentGuardLocation)) {
+
+  for (
+    const [currentGuardLocation] of guardLocations(
+      grid,
+      guardStartLocation,
+      "up",
+    )
+  ) {
     visitedLocations.add(currentGuardLocation.toString());
-    while (true) {
-      const nextLocation = currentGuardLocation.relative(
-        currentGuardDirection,
-        1,
-      );
-      if (grid.get(nextLocation) === "#") {
-        currentGuardDirection = clockwise(currentGuardDirection);
-      } else {
-        currentGuardLocation = nextLocation;
-        break;
-      }
-    }
   }
   return visitedLocations.size;
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function doesPathLoopFromPosition(
+  grid: FixedSizeGrid<string>,
+  existingDirectionsTraveledPerCell: Grid<Set<Direction>>,
+  guardLocation: Location,
+  guardDirection: Direction,
+): boolean {
+  const tempDirectionsTraveledPerCell = new ArrayGrid<Set<Direction>>(
+    grid.dimensions,
+    Array.from(
+      { length: grid.dimensions.rows },
+      () => Array.from({ length: grid.dimensions.columns }, () => new Set()),
+    ),
+  );
+  for (
+    const [currentGuardLocation, currentGuardDirection] of guardLocations(
+      grid,
+      guardLocation,
+      guardDirection,
+    )
+  ) {
+    const directionsTraveledInCurrentCell = tempDirectionsTraveledPerCell.get(
+      currentGuardLocation,
+    )!;
+    if (
+      directionsTraveledInCurrentCell.has(currentGuardDirection) ||
+      existingDirectionsTraveledPerCell.get(currentGuardLocation)!
+        .has(currentGuardDirection)
+    ) {
+      return true;
+    }
+    directionsTraveledInCurrentCell.add(currentGuardDirection);
+  }
+  return false;
+}
+
+function part2(input: string): number {
+  const inputLines = input.trimEnd().split("\n");
+  const grid = new ArrayGrid(
+    new Vector(inputLines.length, inputLines[0].length),
+    inputLines.map((line) => Array.from(line)),
+  );
+
+  const guardStartLocation = grid.valuesWithLocations()
+    .find(({ value }) => value === "^")?.location;
+  if (!guardStartLocation) {
+    throw new Error("No guard location found");
+  }
+
+  const directionsTraveledPerCell = new ArrayGrid<Set<Direction>>(
+    grid.dimensions,
+    Array.from(
+      { length: grid.dimensions.rows },
+      () => Array.from({ length: grid.dimensions.columns }, () => new Set()),
+    ),
+  );
+
+  const possibleObstructionLocations = new Set<string>();
+  for (
+    const [currentGuardLocation, currentGuardDirection] of guardLocations(
+      grid,
+      guardStartLocation,
+      "up",
+    )
+  ) {
+    const directionsTraveledInCurrentCell = directionsTraveledPerCell.get(
+      currentGuardLocation,
+    )!;
+    directionsTraveledInCurrentCell.add(currentGuardDirection);
+
+    const nextLocation = currentGuardLocation.relative(
+      currentGuardDirection,
+      1,
+    );
+    const nextLocationValue = grid.get(nextLocation);
+    if (
+      nextLocationValue !== "#" &&
+      nextLocationValue !== undefined &&
+      !possibleObstructionLocations.has(nextLocation.toString()) &&
+      directionsTraveledPerCell.get(nextLocation)!.size === 0
+    ) {
+      // test putting an obstruction ahead of the guard
+      grid.set(nextLocation, "#");
+      if (
+        doesPathLoopFromPosition(
+          grid,
+          directionsTraveledPerCell,
+          currentGuardLocation,
+          clockwise(currentGuardDirection),
+        )
+      ) {
+        possibleObstructionLocations.add(nextLocation.toString());
+      }
+      grid.set(nextLocation, nextLocationValue);
+    }
+  }
+  return possibleObstructionLocations.size;
+}
 
 if (import.meta.main) {
   runPart(2024, 6, 1, part1);
-  // runPart(2024, 6, 2, part2);
+  runPart(2024, 6, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -57,6 +165,62 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 41);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 6);
+});
+
+Deno.test("part2 immediate obstruction", () => {
+  assertEquals(
+    part2(`\
+.....#....
+.....#....
+..........
+...#^#....
+....##....
+..........
+`),
+    2,
+  );
+});
+
+Deno.test("part2 don't double-count possible obstruction", () => {
+  assertEquals(
+    part2(`\
+....##....
+........#.
+..........
+....^..#..
+...#......
+......#...
+`),
+    3,
+  );
+});
+
+Deno.test("part2 don't allow obstruction on traveled path", () => {
+  assertEquals(
+    part2(`\
+....##....
+........#.
+..........
+.......#..
+..........
+....^.....
+`),
+    1,
+  );
+});
+
+Deno.test("part2 obstruction can be re-encountered", () => {
+  assertEquals(
+    part2(`\
+..#.......
+....O.....
+........#.
+.#........
+...#...#..
+....^.....
+`),
+    1,
+  );
+});
