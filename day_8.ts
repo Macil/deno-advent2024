79507e87e36353debf6502f37208cfbce6b130bf
Chris Cowan <agentme49@gmail.com>
2025-01-02 00:36:42 -0800

    day 8 part 2

--- day_8.ts
+++ day_8.ts
@@ -36,14 +36,47 @@ function part1(input: string): number {
   return antinodeLocations.size;
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function part2(input: string): number {
+  const grid = CharacterGrid.fromString(input);
+
+  const nodeLocationsByFrequency: Partial<Record<string, Location[]>> =
+    mapEntries(
+      Object.groupBy(
+        grid.valuesWithLocations().filter(({ value }) => value !== "."),
+        ({ value }) => value,
+      ),
+      (
+        [frequency, entries],
+      ) => [frequency, entries?.map(({ location }) => location)],
+    );
+
+  const antinodeLocations = new Set<string>();
+
+  for (const locations of Object.values(nodeLocationsByFrequency)) {
+    if (!locations) continue;
+    const allPairs = permutationSet(locations, 2);
+    for (const [a, b] of allPairs) {
+      const difference = b.subtract(a);
+      let antinodeLocation = b;
+      while (grid.isInBounds(antinodeLocation)) {
+        antinodeLocations.add(antinodeLocation.toString());
+        antinodeLocation = antinodeLocation.add(difference);
+      }
+      const oppositeDifference = difference.scale(-1);
+      antinodeLocation = a;
+      while (grid.isInBounds(antinodeLocation)) {
+        antinodeLocations.add(antinodeLocation.toString());
+        antinodeLocation = antinodeLocation.add(oppositeDifference);
+      }
+    }
+  }
+
+  return antinodeLocations.size;
+}
 
 if (import.meta.main) {
   runPart(2024, 8, 1, part1);
-  // runPart(2024, 8, 2, part2);
+  runPart(2024, 8, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -65,6 +98,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 14);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 34);
+});
