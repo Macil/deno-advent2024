f82ed63edb24f3c0543cd4933461173d4fecd1aa
Chris Cowan <agentme49@gmail.com>
2025-01-02 02:16:41 -0800

    day 10 part 1

---
+++ day_10.ts
@@ -0,0 +1,77 @@
+import { assertEquals } from "@std/assert";
+import { dijkstraAll } from "lazy-pathfinding/directed/dijkstra";
+import { runPart } from "@macil/aocd";
+import { ArrayGrid } from "./grid/grid.ts";
+import { orthogonalNeighbors, Vector } from "./grid/vector.ts";
+import { Location } from "./grid/location.ts";
+
+function parse(input: string): ArrayGrid<number | undefined> {
+  const data = input.trimEnd().split("\n").map((line) =>
+    Array.from(line).map((chr) => (chr === "." ? undefined : Number(chr)))
+  );
+  return new ArrayGrid(new Vector(data.length, data[0].length), data);
+}
+
+function getTrailheads(
+  grid: ArrayGrid<number | undefined>,
+): IteratorObject<Location> {
+  return grid.valuesWithLocations()
+    .filter(({ value }) => value === 0)
+    .map(({ location }) => location);
+}
+
+function scoreTrailhead(
+  grid: ArrayGrid<number | undefined>,
+  trailhead: Location,
+): number {
+  const encounteredNodes = dijkstraAll({
+    start: trailhead,
+    successors: (location) =>
+      orthogonalNeighbors()
+        .map((neighbor): [Location, number] => [location.add(neighbor), 1])
+        .filter(([neighborLocation]) =>
+          grid.get(neighborLocation) === grid.get(location)! + 1
+        ),
+    key: (location) => location.toString(),
+  });
+  return encounteredNodes.values()
+    .filter(({ node }) => grid.get(node) === 9)
+    .reduce((sum) => sum + 1, 0);
+}
+
+function part1(input: string): number {
+  const grid = parse(input);
+  const trailheads = getTrailheads(grid);
+  return trailheads
+    .map((trailhead) => scoreTrailhead(grid, trailhead))
+    .reduce((a, b) => a + b, 0);
+}
+
+// function part2(input: string): number {
+//   const grid = parse(input);
+//   throw new Error("TODO");
+// }
+
+if (import.meta.main) {
+  runPart(2024, 10, 1, part1);
+  // runPart(2024, 10, 2, part2);
+}
+
+const TEST_INPUT = `\
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 36);
+});
+
+// Deno.test("part2", () => {
+//   assertEquals(part2(TEST_INPUT), 12);
+// });
