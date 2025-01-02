6119ec9c7baf753bc13a36d141c0c27fb9d07ec0
Chris Cowan <agentme49@gmail.com>
2025-01-02 00:31:32 -0800

    day 8 part 1

---
+++ day_8.ts
@@ -0,0 +1,70 @@
+import { assertEquals } from "@std/assert";
+import { mapEntries } from "@std/collections/map-entries";
+import { permutationSet } from "@hugoalh/setation/set";
+import { runPart } from "@macil/aocd";
+import { CharacterGrid } from "./grid/grid.ts";
+import { Location } from "./grid/location.ts";
+
+function part1(input: string): number {
+  const grid = CharacterGrid.fromString(input);
+
+  const nodeLocationsByFrequency: Partial<Record<string, Location[]>> =
+    mapEntries(
+      Object.groupBy(
+        grid.valuesWithLocations().filter(({ value }) => value !== "."),
+        ({ value }) => value,
+      ),
+      (
+        [frequency, entries],
+      ) => [frequency, entries?.map(({ location }) => location)],
+    );
+
+  const antinodeLocations = new Set<string>();
+
+  for (const locations of Object.values(nodeLocationsByFrequency)) {
+    if (!locations) continue;
+    const allPairs = permutationSet(locations, 2);
+    for (const [a, b] of allPairs) {
+      const difference = b.subtract(a);
+      const antinodeLocation = b.add(difference);
+      if (grid.isInBounds(antinodeLocation)) {
+        antinodeLocations.add(antinodeLocation.toString());
+      }
+    }
+  }
+
+  return antinodeLocations.size;
+}
+
+// function part2(input: string): number {
+//   const items = parse(input);
+//   throw new Error("TODO");
+// }
+
+if (import.meta.main) {
+  runPart(2024, 8, 1, part1);
+  // runPart(2024, 8, 2, part2);
+}
+
+const TEST_INPUT = `\
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 14);
+});
+
+// Deno.test("part2", () => {
+//   assertEquals(part2(TEST_INPUT), 12);
+// });
