19a409032f9c47084d08fd2673f08d4c085428be
Chris Cowan <agentme49@gmail.com>
2025-11-22 23:03:08 -0800

    day 16 part 1

---
+++ day_16.ts
@@ -0,0 +1,138 @@
+import { assertEquals } from "@std/assert";
+import { runPart } from "@macil/aocd";
+import { Location } from "./grid/location.ts";
+import { CharacterGrid, FixedSizeGrid } from "./grid/grid.ts";
+import { aStar } from "lazy-pathfinding/directed/a_star";
+import { clockwise, counterclockwise, Direction } from "./grid/direction.ts";
+
+interface Maze {
+  grid: CharacterGrid;
+  start: Location;
+  end: Location;
+}
+
+interface PathNode {
+  location: Location;
+  direction: Direction;
+}
+
+function parse(input: string): Maze {
+  const grid = CharacterGrid.fromString(input);
+  const start = grid.valuesWithLocations().find(({ value }) => value === "S")
+    ?.location;
+  const end = grid.valuesWithLocations().find(({ value }) => value === "E")
+    ?.location;
+  if (!start || !end) {
+    throw new Error("Missing start or end location");
+  }
+  return { grid, start, end };
+}
+
+function findMinScoreBetweenLocations(
+  grid: FixedSizeGrid<string>,
+  from: Location,
+  to: Location,
+): number {
+  function manhattanDistance(a: Location, b: Location): number {
+    return Math.abs(a.row - b.row) + Math.abs(a.column - b.column);
+  }
+
+  const result = aStar<PathNode>({
+    start: {
+      location: from,
+      direction: "right",
+    },
+    *successors(node) {
+      const forwardLocation = node.location.relative(node.direction, 1);
+      const forwardValue = grid.get(forwardLocation);
+      if (forwardValue && forwardValue !== "#") {
+        yield [{
+          location: forwardLocation,
+          direction: node.direction,
+        }, 1];
+      }
+      yield [{
+        location: node.location,
+        direction: clockwise(node.direction),
+      }, 1000];
+      yield [{
+        location: node.location,
+        direction: counterclockwise(node.direction),
+      }, 1000];
+    },
+    // TODO could factor in direction too
+    heuristic(node) {
+      return manhattanDistance(node.location, to);
+    },
+    success: (node) => node.location.equals(to),
+    key: (node) => node.location.toString() + ":" + node.direction,
+  });
+
+  if (!result) {
+    throw new Error("No path found");
+  }
+
+  return result![1];
+}
+
+function part1(input: string): number {
+  const maze = parse(input);
+  return findMinScoreBetweenLocations(maze.grid, maze.start, maze.end);
+}
+
+// function part2(input: string): number {
+//   const maze = parse(input);
+//   throw new Error("TODO");
+// }
+
+if (import.meta.main) {
+  runPart(2024, 16, 1, part1);
+  // runPart(2024, 16, 2, part2);
+}
+
+const TEST_INPUT = `\
+###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############
+`;
+
+const TEST_INPUT2 = `\
+#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 7036);
+  assertEquals(part1(TEST_INPUT2), 11048);
+});
+
+// Deno.test("part2", () => {
+//   assertEquals(part2(TEST_INPUT), 12);
+// });
