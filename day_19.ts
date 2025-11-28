8b4ebf10fcc27053bac99c92eb5038762780e42e
Chris Cowan <agentme49@gmail.com>
2025-11-27 22:10:12 -0800

    progress on day 19 part 2. still too slow for real input

--- day_19.ts
+++ day_19.ts
@@ -1,6 +1,10 @@
 import { assertEquals } from "@std/assert";
 import { runPart } from "@macil/aocd";
-import { aStar } from "@macil/pathfinding/directed/a_star";
+import {
+  aStar,
+  aStarBag,
+  AStarOptions,
+} from "@macil/pathfinding/directed/a_star";
 
 interface Setup {
   patterns: string[];
@@ -15,21 +19,28 @@ function parse(input: string): Setup {
   return { patterns, designs };
 }
 
-function isDesignSolvable(design: string, patterns: string[]): boolean {
-  const result = aStar<string>({
+function makeAStarOptions(
+  design: string,
+  patterns: string[],
+): AStarOptions<string> {
+  return {
     start: "",
     *successors(current) {
       for (const pattern of patterns) {
         const next = current + pattern;
         if (design.startsWith(next)) {
-          yield [next, 1];
+          yield [next, pattern.length];
         }
       }
     },
     heuristic: (current) => design.length - current.length,
     success: (current) => current === design,
     key: (current) => current,
-  });
+  };
+}
+
+function isDesignSolvable(design: string, patterns: string[]): boolean {
+  const result = aStar(makeAStarOptions(design, patterns));
   return result !== undefined;
 }
 
@@ -40,10 +51,20 @@ function part1(input: string): number {
     .length;
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function countSolutions(design: string, patterns: string[]): number {
+  const bag = aStarBag(makeAStarOptions(design, patterns));
+  if (!bag) {
+    return 0;
+  }
+  return bag[0].reduce((count, _) => count + 1, 0);
+}
+
+function part2(input: string): number {
+  const setup = parse(input);
+  return setup.designs
+    .map((design) => countSolutions(design, setup.patterns))
+    .reduce((sum, count) => sum + count, 0);
+}
 
 if (import.meta.main) {
   runPart(2024, 19, 1, part1);
@@ -67,6 +88,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 6);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 16);
+});
