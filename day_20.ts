c67fce10b7c0b9e3b639fc4b988abcb63198bfa4
Chris Cowan <agentme49@gmail.com>
2025-11-28 02:19:24 -0800

    day 20 part 2

--- day_20.ts
+++ day_20.ts
@@ -5,6 +5,7 @@ import { Location } from "./grid/location.ts";
 import { orthogonalNeighbors } from "./grid/vector.ts";
 import { dijkstraAll } from "@macil/pathfinding/directed/dijkstra";
 import { minBy } from "@std/collections/min-by";
+import { locationsInManhattanDistance } from "./grid/locationsInManhattanDistance.ts";
 
 interface Map {
   grid: FixedSizeGrid<string>;
@@ -61,7 +62,7 @@ function countCheatsThatSaveAtLeast(
     key: (location) => location.toString(),
   });
 
-  const allCheats: IteratorObject<
+  const cheatsWithinTargetTime: IteratorObject<
     { cheatUsed: { skippedWall: Location; to: Location }; timeSaved: number }
   > = map.grid.valuesWithLocations()
     .filter(({ location, value }) =>
@@ -76,11 +77,7 @@ function countCheatsThatSaveAtLeast(
           const value = map.grid.get(loc);
           return value !== undefined && value !== "#";
         })
-        .filter((loc) => {
-          const locString = loc.toString();
-          return forwardDijkstraAllResults.has(locString) &&
-            reverseDijkstraAllResults.has(locString);
-        });
+        .filter((loc) => forwardDijkstraAllResults.has(loc.toString()));
 
       if (openNeighbors.length < 2) {
         return;
@@ -96,9 +93,12 @@ function countCheatsThatSaveAtLeast(
       for (const neighbor of openNeighbors) {
         if (neighbor === closestToStart) continue;
         const reverseDijkstraNode = reverseDijkstraAllResults
-          .get(neighbor.toString())!;
-        const timeSaved = noCheatTime - (closestToStartDijkstraNode.cost +
-          reverseDijkstraNode.cost + 2);
+          .get(neighbor.toString());
+        if (!reverseDijkstraNode) continue;
+
+        const totalSolutionTime = closestToStartDijkstraNode.cost +
+          reverseDijkstraNode.cost + 2;
+        const timeSaved = noCheatTime - totalSolutionTime;
 
         if (timeSaved < targetSaveTime) continue;
 
@@ -109,7 +109,7 @@ function countCheatsThatSaveAtLeast(
       }
     });
 
-  return allCheats.reduce((count) => count + 1, 0);
+  return cheatsWithinTargetTime.reduce((count) => count + 1, 0);
 }
 
 function part1(input: string, targetSaveTime = 100): number {
@@ -117,14 +117,102 @@ function part1(input: string, targetSaveTime = 100): number {
   return countCheatsThatSaveAtLeast(map, targetSaveTime);
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+const CHEAT_TIME_PART2 = 20;
+
+function countCheatsThatSaveAtLeastPart2(
+  map: Map,
+  targetSaveTime: number,
+): number {
+  const forwardDijkstraAllResults = dijkstraAll<Location, number, string>({
+    start: map.start,
+    successors(location) {
+      return orthogonalNeighbors()
+        .map((neighbor): [Location, number] => [location.add(neighbor), 1])
+        .filter(([loc]) => {
+          const value = map.grid.get(loc);
+          return value !== undefined && value !== "#";
+        });
+    },
+    key: (location) => location.toString(),
+  });
+
+  const endEncounteredNode = forwardDijkstraAllResults
+    .get(map.end.toString());
+  if (!endEncounteredNode) {
+    throw new Error("No path found");
+  }
+  const noCheatTime = endEncounteredNode.cost;
+
+  const reverseDijkstraAllResults = dijkstraAll<Location, number, string>({
+    start: map.end,
+    successors(location) {
+      return orthogonalNeighbors()
+        .map((neighbor): [Location, number] => [location.add(neighbor), 1])
+        .filter(([loc]) => {
+          const value = map.grid.get(loc);
+          return value !== undefined && value !== "#";
+        });
+    },
+    key: (location) => location.toString(),
+  });
+
+  const possibleCheatStartLocations: IteratorObject<Location> = map.grid
+    .valuesWithLocations()
+    .filter(({ value, location }) =>
+      value !== "#" &&
+      !location.equals(map.end) &&
+      forwardDijkstraAllResults.has(location.toString())
+    )
+    .map(({ location }) => location);
+
+  const cheatsWithinTargetTime: IteratorObject<
+    { cheatUsed: { start: Location; to: Location }; timeSaved: number }
+  > = possibleCheatStartLocations
+    .flatMap(function* (start) {
+      const startDijkstraNode = forwardDijkstraAllResults
+        .get(start.toString())!;
+
+      for (
+        const endCandidateLocation of locationsInManhattanDistance(
+          start,
+          CHEAT_TIME_PART2,
+        )
+      ) {
+        if (endCandidateLocation.equals(start)) continue;
+        const endCandidateValue = map.grid.get(endCandidateLocation);
+        if (endCandidateValue === undefined || endCandidateValue === "#") {
+          continue;
+        }
+        const reverseDijkstraNode = reverseDijkstraAllResults
+          .get(endCandidateLocation.toString());
+        if (!reverseDijkstraNode) {
+          continue;
+        }
+        const totalSolutionTime = startDijkstraNode.cost +
+          reverseDijkstraNode.cost +
+          start.subtract(endCandidateLocation).l1Norm();
+        const timeSaved = noCheatTime - totalSolutionTime;
+
+        if (timeSaved < targetSaveTime) continue;
+
+        yield {
+          cheatUsed: { start, to: endCandidateLocation },
+          timeSaved,
+        };
+      }
+    });
+
+  return cheatsWithinTargetTime.reduce((count) => count + 1, 0);
+}
+
+function part2(input: string, targetSaveTime = 100): number {
+  const map = parse(input);
+  return countCheatsThatSaveAtLeastPart2(map, targetSaveTime);
+}
 
 if (import.meta.main) {
   runPart(2024, 20, 1, part1);
-  // runPart(2024, 20, 2, part2);
+  runPart(2024, 20, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -159,6 +247,19 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT, 2), 44);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT, 76), 3);
+  assertEquals(part2(TEST_INPUT, 74), 7);
+  assertEquals(part2(TEST_INPUT, 72), 29);
+  assertEquals(part2(TEST_INPUT, 70), 41);
+  assertEquals(part2(TEST_INPUT, 68), 55);
+  assertEquals(part2(TEST_INPUT, 66), 67);
+  assertEquals(part2(TEST_INPUT, 64), 86);
+  assertEquals(part2(TEST_INPUT, 62), 106);
+  assertEquals(part2(TEST_INPUT, 60), 129);
+  assertEquals(part2(TEST_INPUT, 58), 154);
+  assertEquals(part2(TEST_INPUT, 56), 193);
+  assertEquals(part2(TEST_INPUT, 54), 222);
+  assertEquals(part2(TEST_INPUT, 52), 253);
+  assertEquals(part2(TEST_INPUT, 50), 285);
+});
