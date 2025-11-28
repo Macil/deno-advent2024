69d9e7df7de6209bb108ad665c63ed3d00454a32
Chris Cowan <agentme49@gmail.com>
2025-11-28 00:53:01 -0800

    day 20 part 1

---
+++ day_20.ts
@@ -0,0 +1,183 @@
+import { assertEquals } from "@std/assert";
+import { runPart } from "@macil/aocd";
+import { CharacterGrid, FixedSizeGrid } from "./grid/grid.ts";
+import { Location } from "./grid/location.ts";
+import { orthogonalNeighbors } from "./grid/vector.ts";
+import { dijkstraAll } from "@macil/pathfinding/directed/dijkstra";
+import { minBy } from "@std/collections/min-by";
+
+interface Map {
+  grid: FixedSizeGrid<string>;
+  start: Location;
+  end: Location;
+}
+
+function parse(input: string): Map {
+  const grid = CharacterGrid.fromString(input);
+  const start = grid.valuesWithLocations().find(({ value }) => value === "S")
+    ?.location;
+  const end = grid.valuesWithLocations().find(({ value }) => value === "E")
+    ?.location;
+  if (!start || !end) {
+    throw new Error("Missing start or end location");
+  }
+  return { grid, start, end };
+}
+
+function countCheatsThatSaveAtLeast(
+  map: Map,
+  targetSaveTime: number,
+): number {
+  // const noCheatResult = aStar<Location>({
+  //   start: map.start,
+  //   successors(location) {
+  //     return orthogonalNeighbors()
+  //       .map((neighbor): [Location, number] => [location.add(neighbor), 1])
+  //       .filter(([loc]) => {
+  //         const value = map.grid.get(loc);
+  //         return value !== undefined && value !== "#";
+  //       });
+  //   },
+  //   heuristic: (location) => location.subtract(map.end).l1Norm(),
+  //   success: (location) => location.equals(map.end),
+  //   key: (location) => location.toString(),
+  // });
+  // if (!noCheatResult) {
+  //   throw new Error("No path found");
+  // }
+  // const noCheatTime = noCheatResult[1];
+
+  const forwardDijkstraAllResults = dijkstraAll<Location, number, string>({
+    start: map.start,
+    successors(location) {
+      return orthogonalNeighbors()
+        .map((neighbor): [Location, number] => [location.add(neighbor), 1])
+        .filter(([loc]) => {
+          const value = map.grid.get(loc);
+          return value !== undefined && value !== "#";
+        });
+    },
+    key: (location) => location.toString(),
+  });
+
+  const endEncounteredNode = forwardDijkstraAllResults
+    .get(map.end.toString());
+  if (!endEncounteredNode) {
+    throw new Error("No path found");
+  }
+  const noCheatTime = endEncounteredNode.cost;
+
+  const reverseDijkstraAllResults = dijkstraAll<Location, number, string>({
+    start: map.end,
+    successors(location) {
+      return orthogonalNeighbors()
+        .map((neighbor): [Location, number] => [location.add(neighbor), 1])
+        .filter(([loc]) => {
+          const value = map.grid.get(loc);
+          return value !== undefined && value !== "#";
+        });
+    },
+    key: (location) => location.toString(),
+  });
+
+  const allCheats: IteratorObject<
+    { cheatUsed: { skippedWall: Location; to: Location }; timeSaved: number }
+  > = map.grid.valuesWithLocations()
+    .filter(({ location, value }) =>
+      value === "#" &&
+      location.row > 0 && location.row < map.grid.dimensions.rows - 1 &&
+      location.column > 0 && location.column < map.grid.dimensions.columns - 1
+    )
+    .flatMap(function* ({ location }) {
+      const openNeighbors = orthogonalNeighbors()
+        .map((dir) => location.add(dir))
+        .filter((loc) => {
+          const value = map.grid.get(loc);
+          return value !== undefined && value !== "#";
+        })
+        .filter((loc) => {
+          const locString = loc.toString();
+          return forwardDijkstraAllResults.has(locString) &&
+            reverseDijkstraAllResults.has(locString);
+        });
+
+      if (openNeighbors.length < 2) {
+        return;
+      }
+
+      const closestToStart = minBy(
+        openNeighbors,
+        (neighbor) => forwardDijkstraAllResults.get(neighbor.toString())!.cost,
+      )!;
+      const closestToStartDijkstraNode = forwardDijkstraAllResults
+        .get(closestToStart.toString())!;
+
+      for (const neighbor of openNeighbors) {
+        if (neighbor === closestToStart) continue;
+        const reverseDijkstraNode = reverseDijkstraAllResults
+          .get(neighbor.toString())!;
+        const timeSaved = noCheatTime - (closestToStartDijkstraNode.cost +
+          reverseDijkstraNode.cost + 2);
+
+        if (timeSaved < targetSaveTime) continue;
+
+        yield {
+          cheatUsed: { skippedWall: location, to: neighbor },
+          timeSaved,
+        };
+      }
+    });
+
+  return allCheats.reduce((count) => count + 1, 0);
+}
+
+function part1(input: string, targetSaveTime = 100): number {
+  const map = parse(input);
+  return countCheatsThatSaveAtLeast(map, targetSaveTime);
+}
+
+// function part2(input: string): number {
+//   const items = parse(input);
+//   throw new Error("TODO");
+// }
+
+if (import.meta.main) {
+  runPart(2024, 20, 1, part1);
+  // runPart(2024, 20, 2, part2);
+}
+
+const TEST_INPUT = `\
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT, 64), 1);
+  assertEquals(part1(TEST_INPUT, 40), 2);
+  assertEquals(part1(TEST_INPUT, 38), 3);
+  assertEquals(part1(TEST_INPUT, 36), 4);
+  assertEquals(part1(TEST_INPUT, 20), 5);
+  assertEquals(part1(TEST_INPUT, 12), 8);
+  assertEquals(part1(TEST_INPUT, 10), 10);
+  assertEquals(part1(TEST_INPUT, 8), 14);
+  assertEquals(part1(TEST_INPUT, 6), 16);
+  assertEquals(part1(TEST_INPUT, 4), 30);
+  assertEquals(part1(TEST_INPUT, 2), 44);
+});
+
+// Deno.test("part2", () => {
+//   assertEquals(part2(TEST_INPUT), 12);
+// });
