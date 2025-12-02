9f4afbec4df514636ac0190733cfff31ea2b7ff0
Chris Cowan <agentme49@gmail.com>
2025-12-01 23:14:57 -0800

    day 25 part 1

--- day_25.ts
+++ day_25.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert";
-// import { runPart } from "@macil/aocd";
+import { runPart } from "@macil/aocd";
 import { CharacterGrid } from "./grid/grid.ts";
 import { Location } from "./grid/location.ts";
 import { rangeIterator } from "@hugoalh/range-iterator";
@@ -48,18 +48,28 @@ function parse(input: string): Configuration {
     }, { locks: [], keys: [] });
 }
 
-// function part1(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function doKeyAndLockMatch(lock: number[], key: number[]): boolean {
+  return lock.every((lockHeight, col) => key[col] <= 5 - lockHeight);
+}
+
+function part1(input: string): number {
+  const configuration = parse(input);
+  return Iterator.from(configuration.locks)
+    .flatMap((lock) =>
+      Iterator.from(configuration.keys)
+        .map((key): [number[], number[]] => [lock, key])
+    )
+    .filter(([lock, key]) => doKeyAndLockMatch(lock, key))
+    .reduce((count) => count + 1, 0);
+}
 
 // function part2(input: string): number {
-//   const items = parse(input);
+//   const configuration = parse(input);
 //   throw new Error("TODO");
 // }
 
 if (import.meta.main) {
-  // runPart(2024, 25, 1, part1);
+  runPart(2024, 25, 1, part1);
   // runPart(2024, 25, 2, part2);
 }
 
@@ -119,9 +129,9 @@ Deno.test("parse", () => {
   });
 });
 
-// Deno.test("part1", () => {
-//   assertEquals(part1(TEST_INPUT), 3);
-// });
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 3);
+});
 
 // Deno.test("part2", () => {
 //   assertEquals(part2(TEST_INPUT), 12);
